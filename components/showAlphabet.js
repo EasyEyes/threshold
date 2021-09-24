899d88893fd5d9b778245ff4ff6184407624db34
Peiling Jiang <25191575+peilingjiang@users.noreply.github.com>
2021-09-24 00:36:54 -0400

    display valid ans at defined pos

---
+++ components/showAlphabet.js
@@ -0,0 +1,12 @@
+export function getAlphabetShowPos(showWhere) {
+  switch (showWhere) {
+    case "bottom":
+      return [0, -window.innerHeight * 0.3];
+    default:
+      return [0, -window.innerHeight * 0.3];
+  }
+}
+
+export function getAlphabetShowText(valid) {
+  return valid.join(" ");
+}
