038d312cd9f9a971e98eeefd53197479f054db46
Peiling Jiang <25191575+peilingjiang@users.noreply.github.com>
2021-09-26 19:52:29 -0400

    trigger deploy

--- components/instructions.js
+++ components/instructions.js
@@ -3,5 +3,5 @@ export const instructions = {
   initialByThresholdParameter: {
     spacing: `When you see three letters, please report just the middle letter.\n`,
   },
-  initialEnd: `Sometimes the letters will be easy to identify. Sometimes they will be nearly impossible.\nYou can't get much more than half right, so relax. Think of it as a guessing game,\nand just get as many as you can.\nType slowly. (Quit anytime by pressing ESCAPE.)\nLook in the middle of the screen, ignoring the edges of the screen.\nTo continue, please hit RETURN.\n`,
+  initialEnd: `Sometimes the letters will be easy to identify. Sometimes they will be nearly impossible.\nYou can't get much more than half right, so relax. Think of it as a guessing game,\nand just get as many as you can.\nType slowly. (Quit anytime by pressing ESCAPE.)\nLook at the middle of the screen, ignoring the edges of the screen.\nTo continue, please hit RETURN.\n`,
 };
