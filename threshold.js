8363b36e01b6aa4092df21a5004157bd297158b2
Peiling Jiang <25191575+peilingjiang@users.noreply.github.com>
2022-02-03 18:29:29 -0800

    threshold: update rc

--- threshold.js
+++ threshold.js
@@ -2723,67 +2723,66 @@ const isPavloviaExperiment = () => {
 };
 
 const initializeEscHandlingDiv = () => {
+  // TODO This could be improved by a lot
+  // Fixed for old code by @sagarrpandav
   if (document.getElementById("esc-key-handling-div").children.length == 0) {
-    // add only if not present
-    document.getElementById("esc-key-handling-div").innerHTML =
-      "<div\n" +
-      '        class="modal fade"\n' +
-      '        id="exampleModal"\n' +
-      '        tabindex="-1"\n' +
-      '        aria-labelledby="exampleModalLabel"\n' +
-      '        aria-hidden="true"\n' +
-      "      >\n" +
-      '        <div class="modal-dialog">\n' +
-      '          <div class="modal-content">\n' +
-      '            <div class="modal-header">\n' +
-      '              <h3 class="modal-title" id="modalTitle">\n' +
-      "                Escape Key was Pressed\n" +
-      "              </h3>\n" +
-      "            </div>\n" +
-      "            <div\n" +
-      '              class="modal-body"\n' +
-      '              id="modalBody"\n' +
-      '              style="display: flex; justify-content: space-around"\n' +
-      "            >\n" +
-      '              <div style="display: grid">\n' +
-      "                <button\n" +
-      '                  type="button"\n' +
-      '                  id="skip-trial-btn"\n' +
-      '                  class="btn btn-outline-secondary"\n' +
-      '                  style="font-size: x-large"\n' +
-      '                  data-bs-dismiss="modal"\n' +
-      "                >\n" +
-      "                  Skip Trial\n" +
-      "                </button>\n" +
-      '                <span style="text-align: center">(Space)</span>\n' +
-      "              </div>\n" +
-      '              <div id="skip-block-div" style="display: grid">\n' +
-      "                <button\n" +
-      '                  type="button"\n' +
-      '                  id="skip-block-btn"\n' +
-      '                  class="btn btn-outline-secondary"\n' +
-      '                  style="font-size: x-large"\n' +
-      '                  data-bs-dismiss="modal"\n' +
-      "                >\n" +
-      "                  Skip Block\n" +
-      "                </button>\n" +
-      '                <span style="text-align: center">(Enter)</span>\n' +
-      "              </div>\n" +
-      '              <div style="display: grid">\n' +
-      "                <button\n" +
-      '                  type="button"\n' +
-      '                  id="quit-btn"\n' +
-      '                  class="btn btn-outline-danger"\n' +
-      '                  style="font-size: x-large"\n' +
-      '                  data-bs-dismiss="modal"\n' +
-      "                >\n" +
-      "                  Quit\n" +
-      "                </button>\n" +
-      '                <span style="text-align: center">(Escape)</span>\n' +
-      "              </div>\n" +
-      "            </div>\n" +
-      "          </div>\n" +
-      "        </div>\n" +
-      "      </div>";
+    // Add only if not present
+    document.getElementById("esc-key-handling-div").innerHTML = `<div
+  class="modal fade"
+  id="exampleModal"
+  tabindex="-1"
+  aria-labelledby="exampleModalLabel"
+  aria-hidden="true"
+>
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h3 class="modal-title" id="modalTitle">Escape Key was Pressed</h3>
+      </div>
+      <div
+        class="modal-body"
+        id="modalBody"
+        style="display: flex; justify-content: space-around"
+      >
+        <div style="display: grid">
+          <button
+            type="button"
+            id="skip-trial-btn"
+            class="btn btn-outline-secondary"
+            style="font-size: x-large"
+            data-bs-dismiss="modal"
+          >
+            Skip Trial
+          </button>
+          <span style="text-align: center">(Space)</span>
+        </div>
+        <div id="skip-block-div" style="display: grid">
+          <button
+            type="button"
+            id="skip-block-btn"
+            class="btn btn-outline-secondary"
+            style="font-size: x-large"
+            data-bs-dismiss="modal"
+          >
+            Skip Block
+          </button>
+          <span style="text-align: center">(Enter)</span>
+        </div>
+        <div style="display: grid">
+          <button
+            type="button"
+            id="quit-btn"
+            class="btn btn-outline-danger"
+            style="font-size: x-large"
+            data-bs-dismiss="modal"
+          >
+            Quit
+          </button>
+          <span style="text-align: center">(Escape)</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>`;
   }
 };
