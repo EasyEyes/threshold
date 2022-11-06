53e165f90a52c25547cd2aefa770315026809b9d
Augustin Burchell <augustin.burchell@gmail.com>
2022-11-06 17:41:16 -0500

    for threshold: Sort response options in rsvpReading

--- components/rsvpReading.js
+++ components/rsvpReading.js
@@ -175,7 +175,7 @@ export class Category {
     this.target = targetWord;
     this.foils = foils;
     // The list of all possible responses for each set, the target word mixed in with the foils.
-    this.elements = shuffle([this.target, ...this.foils]);
+    this.elements = [this.target, ...this.foils].sort();
   }
 }
 
