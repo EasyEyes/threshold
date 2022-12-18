a5c78d9a88ab1ed1f91a56ed4882181febf38f6e
Peiling Jiang <25191575+peilingjiang@users.noreply.github.com>
2022-12-17 19:55:39 -0800

    for threshold: try using sw to control coop and coep for ffmpeg

---
+++ sw-main.js
@@ -0,0 +1,17 @@
+if ("serviceWorker" in navigator) {
+  // Register service worker
+  navigator.serviceWorker.register(new URL("./sw.js", import.meta.url)).then(
+    function (registration) {
+      console.log("COOP/COEP Service Worker registered", registration.scope);
+      // If the registration is active, but it's not controlling the page
+      if (registration.active && !navigator.serviceWorker.controller) {
+        window.location.reload();
+      }
+    },
+    function (err) {
+      console.log("COOP/COEP Service Worker failed to register", err);
+    }
+  );
+} else {
+  console.warn("Cannot register a service worker");
+}
