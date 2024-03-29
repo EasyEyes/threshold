667a77bc78eae3e27659c6e00f217ca905dc2ea4
Nati Tsegaye <nht249@nyu.edu>
2024-03-29 10:53:53 +0300

    for threshold: rename firebase config

--- components/firebase/firebase.js
+++ components/firebase/firebase.js
@@ -19,7 +19,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig, "sound");
 
 export const db = initializeFirestore(app, {
   experimentalForceLongPolling: true,
