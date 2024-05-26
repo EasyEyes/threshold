0dbd7df79af98e8a6a2d3f30084de0a14054e06c
Nati Tsegaye <nht249@nyu.edu>
2024-05-26 23:37:04 +0300

    for threshold: revert RC package

--- examples/buildExamples.ts
+++ examples/buildExamples.ts
@@ -122,15 +122,6 @@ const constructForEXperiment = async (d: string) => {
         "../recruitmentServiceConfig.csv",
         `${dir}/recruitmentServiceConfig.csv`,
       );
-      copyFileSync(
-        "../RemoteCalibrator.min.js",
-        `${dir}/RemoteCalibrator.min.js`,
-      );
-
-      copyFileSync(
-        "../RemoteCalibrator.min.js.map",
-        `${dir}/RemoteCalibrator.min.js.map`,
-      );
 
       mkdirSync(`${dir}/components`);
       mkdirSync(`${dir}/components/images`);
